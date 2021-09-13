96a9bb2ed3903ce9d2712ea576c1bf537b7db8bd
RoyOtten <99065498@mydavinci.nl>
2021-09-13 10:57:27 +0200

    eerste commit)

---
+++ testloop1.py
@@ -0,0 +1,19 @@
+for i in range(1,11):
+    print (i * 4) 
+
+
+
+for a in range(30, 0, -1):
+    print(str(a))
+
+print("De raket lanceerd")  
+
+for c in range (24):
+    if c >= 13:
+        print (str(c) + "pm")
+    else:
+        print (str(c) + "am")
+
+for x in range(0,50,2): 
+    print(x)
+    
